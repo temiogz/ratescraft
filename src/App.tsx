dc2f46f1b4b6929e19cd193d7b733f45542f2e05
TOPE <topemicheal@yahoo.com>
2023-11-19 05:57:15 -0500

    initial code commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import "./App.css";
+
+
+const App: FC = () => {
+  return (
+    <div className="text-2xl text-slate-500">
+      <h1>web</h1>
+    </div>
+  );
+}
+
+export default App;
